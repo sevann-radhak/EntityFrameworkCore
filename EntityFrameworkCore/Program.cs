f9ae8fcdd928e287e47448282e6900363846b8a3
Sevann Radhak <sevann.radhak@inworx.com>
2020-06-22 22:11:18 -0300

    First commit, first table

---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,43 @@
+﻿using Persistence.Database;
+using Persistence.Database.Models;
+using System;
+
+namespace EntityFrameworkCore
+{
+    class Program
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Program(ApplicationDbContext context)
+        {
+            _context = context;
+            //AddSong();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.Add(new Song
+                {
+                    Description = "First Song Description",
+                    Duration = TimeSpan.FromSeconds(250),
+                    Title = "First Song Title"
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        //private void AddSong()
+        //{
+        //    _context.Add(new Song { 
+        //        Description = "First Song Description",
+        //        Duration = TimeSpan.FromSeconds(250),
+        //        Title = "First Song Title"
+        //    });
+        //}
+    }
+}
---
+++ Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Database.Models;
+
+namespace Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EntityFrameworkCoreMusic;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
